3a69c4011409f73c41ba8f0f50160998daba27c1
Luis Restrepo <luis.restrepo@antpack.co>
2025-12-18 20:28:42 +0000

    File changes

--- src/pages/ProjectChecklist.jsx
+++ src/pages/ProjectChecklist.jsx
@@ -483,6 +483,7 @@ export default function ProjectChecklist() {
                 )}
               </Droppable>
             </DragDropContext>
+            )}
           </div>
           
           {/* Panel lateral - Resumen */}
