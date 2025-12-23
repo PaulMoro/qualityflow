e5314a89f3df096e91b6a5aae61997c57aaa1afe
Luis Restrepo <luis.restrepo@antpack.co>
2025-12-23 17:28:11 +0000

    File changes

--- src/components/workflow/WorkflowTracker.jsx
+++ src/components/workflow/WorkflowTracker.jsx
@@ -353,7 +353,7 @@ export default function WorkflowTracker({ project, userRole }) {
                         <Button
                           size="sm"
                           onClick={() => handleStartPhase(phaseKey)}
-                          className="w-full sm:w-auto"
+                          className="w-full sm:w-auto bg-[#FF1B7E] hover:bg-[#e6156e] text-white"
                         >
                           Iniciar
                         </Button>
